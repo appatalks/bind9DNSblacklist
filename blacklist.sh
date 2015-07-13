9e9f4f4914fe2c722f7af91dc8fe696a7e40eccf
Steven <servingbaby@users.noreply.github.com>
2015-07-13 01:02:25 -0500

    Create blacklist.sh

---
+++ blacklist.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# @hoshisato.com
+# Author: Steven Bennett
+# Contact: steven@stbennett.org
+# 
+# About: This script will add domains (from a text file) to the NAMED blacklist.zone file.
+# 
+# Usage: ./blacklist.sh domains.txt
+############
+
+DOMAIN=$1
+
+cat $1 | while read DOMAIN; do
+	echo "zone "$DOMAIN"  {type master; file "blockeddomain.hosts";};" >> /var/named/blacklist.zones
+	echo "zone "$DOMAIN"  {type master; file "blockeddomain.hosts";};"
+	service named restart
+done
